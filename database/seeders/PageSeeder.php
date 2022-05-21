db770e805ee779027b6076f10754d53ea2c12d7e
wojiaosasha <sasha.79051@gmail.com>
2022-05-22 02:08:42 +1000

    Lesson 2: Add pages (web)

---
+++ database/seeders/PageSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Page;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Page::query()->delete();
+        Page::factory(random_int(20,30))->create();
+    }
+}
