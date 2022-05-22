24f9e469fd588f485199fba99bf3f1ac2c0ac0dc
wojiaosasha <66951136+wojiaosasha@users.noreply.github.com>
2022-05-22 15:39:36 +1000

    Lesson 2: Add pages (web) (#2)
    * Lesson 1: Add Pgadmin config
    
    * Lesson 1: fixed .env.example
    
    * Lesson 2: Add pages (web)
    
    * Lesson 2: Add pages (web)(2)
    
    * Lesson 2: Install "cviebrock/eloquent-sluggable" component

--- database/seeders/PageSeeder.php
+++ database/seeders/PageSeeder.php
@@ -2,21 +2,20 @@
 
 namespace Database\Seeders;
 
+use App\Models\Page;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class PageSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call([
-            PageSeeder::class,
-        ]);
+        Page::query()->delete();
+        Page::factory(random_int(20,30))->create();
     }
 }
